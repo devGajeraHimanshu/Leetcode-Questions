c39e277658222e57330040eab3c19e16410c707d
devGajeraHimanshu <102642804+devGajeraHimanshu@users.noreply.github.com>
2022-10-02 21:07:03 +0530

    Done the changes with explanation

--- Easy/Length_of_last_word.cpp
+++ Easy/Length_of_last_word.cpp
@@ -1,17 +1,33 @@
+// Suppose we have a string s. s can hold any English letters and white-spaces. We have to find the length of last word in the string.
+// If there is no last word, then return 0.
+// 
+// So, if the input is like "I love Programming", then the output will be 11
+// 
+// To solve this, we will follow these steps −
+// 
+// n := 0
+// 
+// for each word temp in a string −
+// 
+// n := size of temp
+// 
+// return n
+
+#include <bits/stdc++.h>
+using namespace std;
 class Solution {
 public:
-    int lengthOfLastWord(string s) {
-        int t=s.length();
-        int c=0;
-        for(int i=t-1;i>=0;i--)
-        {
-            if(s[i]==' '&&c!=0)
-                return c;
-             else if((c==0)&&(s[i]==' '))
-                 continue;
-            else
-                 c++;
-        }
-            return c;
-         }
+   int lengthOfLastWord(string s){
+      stringstream str(s);
+      string temp;
+      int n = 0;
+      while (str >> temp)
+         n = temp.size();
+      return n;
+   }
 };
+
+main(){
+   Solution ob;
+   cout << (ob.lengthOfLastWord("I love Programming"));
+}
